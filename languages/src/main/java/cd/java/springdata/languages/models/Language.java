53d1b379bb3b0de5939d9964a1af01df070a12e2
Caleb A Comstock <c_comstock@rocketmail.com>
2021-04-15 18:17:38 -0700

    created/updated the 'Language' model

--- languages/src/main/java/cd/java/springdata/languages/models/Language.java
+++ languages/src/main/java/cd/java/springdata/languages/models/Language.java
@@ -3,10 +3,156 @@
  */
 package cd.java.springdata.languages.models;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 /**
  * @author ccomstock
  *
  */
-public class Language {
+@Entity
+@Table(name="languages")
+public class Language implements java.io.Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8480711661014475051L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(min = 3, max = 20)
+	private String name;
+	
+	@NotNull
+	@Size(min = 3, max = 20)
+	private String creator;
+	
+	@NotNull
+	private String currentVersion;
+	
+	@Column(updatable = false)
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date createdAt;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date updatedAt;
 
+	public Language() {}
+	
+	public Language(String name, String creator, String currentVersion) {
+		this.name = name;
+		this.creator = creator;
+		this.currentVersion = currentVersion;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the creator
+	 */
+	public String getCreator() {
+		return creator;
+	}
+
+	/**
+	 * @param creator the creator to set
+	 */
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	/**
+	 * @return the currentVersion
+	 */
+	public String getCurrentVersion() {
+		return currentVersion;
+	}
+
+	/**
+	 * @param currentVersion the currentVersion to set
+	 */
+	public void setCurrentVersion(String currentVersion) {
+		this.currentVersion = currentVersion;
+	}
+
+	/**
+	 * @return the createdAt
+	 */
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	/**
+	 * @param createdAt the createdAt (datetime) to set
+	 */
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	/**
+	 * @return the updatedAt
+	 */
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	/**
+	 * @param updatedAt the updatedAt (datetime) to set
+	 */
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	@PrePersist
+	protected void onCreate() {
+		this.createdAt = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.updatedAt = new Date();
+	}
+	
 }
