60f23dea002a98f1d3a1e4881fa452a2b5fe40a4
Caleb A Comstock <c_comstock@rocketmail.com>
2021-04-15 18:30:58 -0700

    created/updated the 'Language' repository

--- languages/src/main/java/cd/java/springdata/languages/repositories/LanguageRepository.java
+++ languages/src/main/java/cd/java/springdata/languages/repositories/LanguageRepository.java
@@ -3,10 +3,41 @@
  */
 package cd.java.springdata.languages.repositories;
 
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import cd.java.springdata.languages.models.Language;
+
 /**
  * @author ccomstock
  *
  */
-public class LanguageRepository {
-
+@Repository
+public interface LanguageRepository extends CrudRepository<Language,Long> {
+	
+	/**
+	 *@return list of all Languages in db
+	 */
+	List<Language> findAll();
+	
+	/**
+	 * @param search the substring to search by
+	 * @return list of all Languages with matching name
+	 */
+	List<Language> findByNameContaining(String search);
+	
+	/**
+	 * @param search the substring to search by
+	 * @return list of all Languages with matching creator
+	 */
+	List<Language> findByCreatorContaining(String search);
+	
+	/**
+	 * @param search the substring to search by
+	 * @return the id of the Language that was found and deleted
+	 */
+	Long deleteByNameStartingWith(String search);
+	
 }
